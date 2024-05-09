59c3972b9314f0d91feed098a73ee3071e26a8f7
Paul Irish <commits@paul.irish>
2024-05-09 08:05:39 -0700

    refactor extract-cpu-profile a tad for reuse

--- extract-cpu-profile-from-trace.mjs
+++ extract-cpu-profile-from-trace.mjs
@@ -6,14 +6,10 @@
 
 
 import fs from 'fs';
+import path from 'node:path';
 import {strict as assert} from 'assert';
-import {saveCpuProfile} from './trace-file-utils.mjs';
+import {saveCpuProfile, loadTraceEventsFromFile} from './trace-file-utils.mjs';
 
-const passedArg = process.argv[2];
-const tracefilename = passedArg ? passedArg : './myjansatta.json';
-let trace = JSON.parse(fs.readFileSync(tracefilename, 'utf-8'));
-let events = trace.traceEvents || trace;
-assert.ok(Array.isArray(events) && events.length);
 
 // A saved .cpuprofile from JS Profiler panel matches `Profiler.Profile` exactly.
 // https://chromedevtools.github.io/devtools-protocol/tot/Profiler/#type-Profile
@@ -25,90 +21,112 @@ assert.ok(Array.isArray(events) && events.length);
 // See also go/cpu-profiler-notes which has plenty more stuff like this.
 
 
-const metaEvts = events.filter(e => e.cat === '__metadata');
+export function extractCPUProfiles(events) {
 
-// Cat = `disabled-by-default-v8.cpu_profiler`
-events = events.filter(e => e.cat.includes('v8.cpu_profiler'));
+    // Cat = `disabled-by-default-v8.cpu_profiler`
+    const metaEvts = events.filter(e => e.cat === '__metadata');
 
-
-console.log(events.length);
-// At this point e.name is either 'ProfileChunk' or 'Profile';
-// ProfileChunk events can be on a diff thread id than the header. but the header is canonical.
-const profileHeadEvts = events.filter(e => e.name === 'Profile');
-// What pid's do we have?
-const pidtids = profileHeadEvts.reduce((prev, curr) => prev.add(`p${curr.pid}t${curr.tid}`), new Set())
-
-// See also `extractCpuProfile` in CDT's TimelineModel
-pidtids.forEach(async pidtid => {
-    const pid = parseInt(pidtid.split('t')[0].replace('p', ''), 10);
-    const tid = parseInt(pidtid.split('t')[1], 10);
-    const threadName = metaEvts.find(e => e.pid === pid && e.tid === tid)?.args.name;
-    console.log(`Looking at: "pid":${pid},"tid":${tid},  …  ${threadName}`);
-
-    const profileHeadEvt = profileHeadEvts.find(e => e.pid === pid && e.tid === tid);
-
-    
-    // flow event connection. id's like 0x2
-    // OKAY this is apparently wrong. but i dont know how these are connected. so weird.
-    const chunkEvts = events.filter(e => e.name === 'ProfileChunk' && e.id === profileHeadEvt.id); 
+    events = events.filter(e => e.cat.includes('v8.cpu_profiler'));
 
 
-    if (!profileHeadEvt) {
-        console.error('missing profile header evt.... probably resolvable but not now');
-        return;
-    }
-    if (!chunkEvts.length){
-        console.error(`No chunk events for ${pidtid}!`);
-        return;
-    } 
+    console.log(events.length);
+    // At this point e.name is either 'ProfileChunk' or 'Profile';
+    // ProfileChunk events can be on a diff thread id than the header. but the header is canonical.
+    const profileHeadEvts = events.filter(e => e.name === 'Profile');
+    // What pid's do we have?
+    const pidtids = profileHeadEvts.reduce((prev, curr) => prev.add(`p${curr.pid}t${curr.tid}`), new Set())
+
+    // See also `extractCpuProfile` in CDT's TimelineModel
+    return Array.from(pidtids).map(async pidtid => {
+        const pid = parseInt(pidtid.split('t')[0].replace('p', ''), 10);
+        const tid = parseInt(pidtid.split('t')[1], 10);
+        const threadName = metaEvts.find(e => e.pid === pid && e.tid === tid)?.args.name;
+        console.log(`Looking at: "pid":${pid},"tid":${tid},  …  ${threadName}`);
+
+        const profileHeadEvt = profileHeadEvts.find(e => e.pid === pid && e.tid === tid);
+
+        // flow event connection. id's like 0x2
+        // OKAY this is apparently wrong. but i dont know how these are connected. so weird.
+        const chunkEvts = events.filter(e => e.name === 'ProfileChunk' && e.id === profileHeadEvt.id); 
+
+        if (!profileHeadEvt) {
+            return console.error('missing profile header evt.... probably resolvable but not now');
+        }
+        if (!chunkEvts.length){
+            return console.error(`No chunk events for ${pidtid}!`);
+        } 
+            
+        /** {Crdp.Profiler.Profile} */
+        const profile = {
+            nodes: [],
+            startTime: -1,
+            endTime: -1,
+            samples: [],
+            timeDeltas: [],
+            ...profileHeadEvt.args.data
+        };
+
+        chunkEvts.forEach(chunk => {
+            const chunkData = chunk.args.data.cpuProfile;
+            profile.nodes.push(... chunkData.nodes || []);
+            profile.samples.push(... chunkData.samples || []);
+            // profile.lines is apparently also a thing (later me. whatttttttttt?) but i dont see that it does anything.. so ignoring for now.
+            // todo delete this comment
+
+
+            // Why is timeDeltas not in .args.data.cpuProfile???? beats me.
+            profile.timeDeltas.push(...  chunk.args.data.timeDeltas || []);
+            // shrug. https://source.chromium.org/chromium/chromium/src/+/main:v8/src/profiler/profile-generator.cc;l=755;bpv=0;bpt=1
+            profile.endTime = chunkData.endTime || profile.endTime;
+        });
         
+        // Stole this from timelinemodel
+        if (profile.endTime === -1){
+            profile.endTime = profile.timeDeltas.reduce((x, y) => x + y, profile.startTime);
+        }
 
-    /** {Crdp.Profiler.Profile} */
-    const profile = {
-        nodes: [],
-        startTime: -1,
-        endTime: -1,
-        samples: [],
-        timeDeltas: [],
-        ...profileHeadEvt.args.data
-    };
+        // for compat with vscode's viewer. 
+        for (const node of profile.nodes) {
+            node.callFrame.url = node.callFrame.url || '';
+        }
 
-    chunkEvts.forEach(chunk => {
-        const chunkData = chunk.args.data.cpuProfile;
-        profile.nodes.push(... chunkData.nodes || []);
-        profile.samples.push(... chunkData.samples || []);
-        // profile.lines is apparently also a thing (later me. whatttttttttt?) but i dont see that it does anything.. so ignoring for now.
-        // todo delete this comment
-
-
-        // Why is timeDeltas not in .args.data.cpuProfile???? beats me.
-        profile.timeDeltas.push(...  chunk.args.data.timeDeltas || []);
-        // shrug. https://source.chromium.org/chromium/chromium/src/+/main:v8/src/profiler/profile-generator.cc;l=755;bpv=0;bpt=1
-        profile.endTime = chunkData.endTime || profile.endTime;
+        return {
+            pid,
+            tid,
+            profile,
+            threadName,
+        }
     });
-    
-    // Stole this from timelinemodel
-    if (profile.endTime === -1){
-        profile.endTime = profile.timeDeltas.reduce((x, y) => x + y, profile.startTime);
-    }
+}
 
-    // for compat with vscode's viewer. 
-    for (const node of profile.nodes) {
-        node.callFrame.url = node.callFrame.url || '';
-    }
 
+
+// CLI direct invocation?
+if (import.meta.url.endsWith(process?.argv[1])) {
+  cli();
+}
+
+async function cli() {
+  const filename = path.resolve(process.cwd(), process.argv[2]);
+
+  const traceEvents = loadTraceEventsFromFile(filename);
+  const profileData = await Promise.all(extractCPUProfiles(traceEvents));
+
+  profileData.forEach(async ({pid, tid, profile, threadName}) => {
     // Uncomment to manually "crop" the cpu profile. (probably dont want this....)
     // profile.samples = profile.samples.slice(0, 50_000);
     // profile.timeDeltas = profile.timeDeltas.slice(0, 50_000);
 
     console.log('counts:', profile.nodes.length, profile.samples.length, profile.timeDeltas.length)
 
-    const filename = `${tracefilename}-pid-${pid}-tid-${tid}-${threadName}.cpuprofile`;
-    
-    // format it
-    await saveCpuProfile(profile, filename);
-    const readRes = fs.readFileSync(filename, 'utf-8');
-    // fs.writeFileSync(filename, JSON.stringify(profile));
-    console.log(`written ${readRes.length.toLocaleString()} bytes to: ${filename}`);
-});
+    const cpuFilename = `${filename}-pid-${pid}-tid-${tid}-${threadName}.cpuprofile`;
 
+
+    // format it
+    await saveCpuProfile(profile, cpuFilename);
+    const readRes = fs.readFileSync(cpuFilename, 'utf-8');
+    // fs.writeFileSync(cpuFilename, JSON.stringify(profile));
+    console.log(`written ${readRes.length.toLocaleString()} bytes to: ${cpuFilename}`);
+  });
+
+}
